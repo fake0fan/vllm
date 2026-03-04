b14cc06f5072acd50cd4a2c6463b36a8c5f3ab74
herotai214 <herotai214@gmail.com>
2026-03-04 02:47:10 +0000

    Fix wait_for_load hang with failed_recving_mm_hashes; (& log + manual edit to mimick cache loss, remove in later commit)
    Signed-off-by: herotai214 <herotai214@gmail.com>

--- vllm/distributed/ec_transfer/ec_connector/example_connector.py
+++ vllm/distributed/ec_transfer/ec_connector/example_connector.py
@@ -89,6 +89,11 @@ class ECExampleConnector(ECConnectorBase):
             if mm_data.mm_hash in encoder_cache:
                 continue
             filename = self._generate_filename_debug(mm_data.mm_hash)
+            logger.debug(f"hero: start_load_caches for {mm_data.mm_hash}, filename: {filename}")
+            # force alter
+            filename = filename + "sss"
+            logger.debug(f"hero: force alter hash file name into {filename}")
+
             ec_cache = safetensors.torch.load_file(
                 filename, device=current_platform.device_type
             )["ec_cache"]
